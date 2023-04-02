1cbb5ac53f3b3eeb1e0e739f4f030992fe66d9b4
Saiful <saifulislamrodro54@gmail.com>
2023-04-02 22:16:31 +0600

    Is prime number

---
+++ PrimeNumber.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num: ");
+        num = sc.nextInt();
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+    }
+   
+}
